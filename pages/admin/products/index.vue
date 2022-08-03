7ea053ef8fbfa22075822e5ee50a6ccde6626ddd
Farzad Farzanehnya <98174497+farzaadweb@users.noreply.github.com>
2022-08-03 22:14:27 +0430

    add new Items & fix bugs

--- pages/admin/products/index.vue
+++ pages/admin/products/index.vue
@@ -1,164 +1,258 @@
 <template>
-    <div class="container-full">
-        <div class="row">
-            <div class="col-lg-3 col-md-3">
-              <!-- Categories -->
-              <section class="CatSec mt-4 py-2 px-4">
-                 <div class="d-flex flex-row align-items-center py-3">
-                  <img src="~/assets/pictures/categorie.svg" class="CatImg" alt="categorie">
-                  <p class="px-2 CatTxt m-0"> Categories</p>               
-                 </div>
-              <!-- categories details -->
-                  <div class="d-flex align-items-center justify-content-between">
-                      <p class="CategorieTxt">Accesories</p>
-                      <p>(5)</p>
-                  </div>
-                  <hr>
-                  <div class="pt-3 d-flex align-items-center justify-content-between">
-                      <p class="CategorieTxt">Face care</p>
-                      <p>(5)</p>
-                  </div>
-                  <hr>
-                  <div class="pt-3 d-flex align-items-center justify-content-between">
-                      <p class="CategorieTxt">Lip care</p>
-                      <p>(5)</p>
-                  </div>
-                  <hr>
-                  <div class="pt-3 d-flex align-items-center justify-content-between">
-                      <p class="CategorieTxt">Skin care</p>
-                      <p>(5)</p>
-                  </div>
-                   <hr>
-                  <div class="pt-3 d-flex align-items-center justify-content-between">
-                      <p class="CategorieTxt">Nail care</p>
-                      <p>(5)</p>
-                  </div>
-                  <hr>
-                  <div class="pt-3 d-flex align-items-center justify-content-between">
-                      <p class="CategorieTxt">Blusher</p>
-                      <p>(5)</p>
-                  </div>
-              </section>
-                <!-- Popular Tags -->
-                <section class="CatSec mt-4 py-2 px-4">
-                 <div class="d-flex flex-row align-items-center py-3">
-                  <img src="~/assets/pictures/categorie.svg" class="CatImg" alt="categorie">
-                  <p class="px-2 CatTxt m-0">Popular Tags</p>               
-                 </div>
-                    <hr>
-              <!-- categories details -->
-                  <div class="d-flex align-items-center justify-content-center">
-                    <button type="button" class="mt-3 mx-2 btn btn-sm btn-light btn-rounded">Beauty</button>
-                    <button type="button" class="mt-3 mx-2 btn btn-sm btn-light btn-rounded">MakeupArtist</button>
-                  </div>
-                  <div class="py-3 d-flex align-items-center justify-content-center">
-                    <button type="button" class="mx-2 btn btn-sm btn-light btn-rounded">Makeup</button>
-                    <button type="button" class="mx-2 btn btn-sm btn-light btn-rounded">Hair</button>
-                    <button type="button" class="mx-2 btn btn-sm btn-light btn-rounded">Nails</button>
-                  </div>
-                   <div class="pb-3 d-flex align-items-center justify-content-center">
-                    <button type="button" class="mx-2 btn btn-sm btn-light btn-rounded">Hairstyle</button>
-                    <button type="button" class="mx-2 btn btn-sm btn-light btn-rounded">Skincare</button>
-                  </div>
-              </section>
-              </div>
-            <div class="col-lg-9 col-md-9 mt-3">
-                <div class="d-flex justify-content-between align-items-center">
-                  <AdminPagination />
-                  <!-- categorie and product btn -->
-                  <div class="d-flex align-items-center">
-                    <button type="button" class="BtnStyleGroup btn btn-sm mx-3">Add New Grouping</button>
-                    <button type="button" class="BtnStyleProd btn btn-sm">Add New Products</button>
-                  </div>
-                </div>
-                 <!-- admin products -->
-                <div class="AdminProd container-ful mt-2 px-4">
-                  <!-- results count -->
-                    <div class="d-flex align-items-center justify-content-between pt-4">
-                  <!-- result number -->    
-                      <div>
-                      <p>Showing 08 Results</p>
-                      </div>
-                  <!-- sort by -->
-                      <div class="d-flex align-items-center">
-                        <p class="sort">Sort</p>
-                        <p class="SortStatus px-2">Last Opened</p>
-                      </div>
-                    </div>
-                  <!-- end result count -->   
-                    <div class="container-full">
-                      <div class="row">
-                        <div class="col-lg-6 col-md-6 p-0 mt-5 px-2">
-                          <section class="ProductBkg">
-                          <div class="d-flex justify-content-between product">
-                            <img src="~/assets/pictures/ProductOne.jpg" class="ProductImg" alt="product">
-                            <div class="ps-2">
-                              <p class="mt-1">Sella body butter</p>
-                              <p class="py-2">Lip Care</p>
-                              <p class="pb-4">$2000</p>
-                              <!-- switch button -->
-                              <div class="d-flex justify-content-between">
-                                <p>inactive</p>
-                                <!-- Checked switch -->
-                                  <div class="form-check form-switch">
-                                    <input class="InputStyle form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked />
-                                  </div>
-                                <!-- end checked switch -->
-                                <p class="ps-3">Active</p>
-                              </div>
-                            </div>
-                          </div>
-                          </section>
-                        </div>
-                        <div class="col-lg-6 col-md-6 p-0 mt-5 px-2">
-                          <section class="ProductBkg">
-                          <div class="d-flex justify-content-between product">
-                            <img src="~/assets/pictures/ProductOne.jpg" class="ProductImg" alt="product">
-                            <div class="ps-2">
-                              <p class="mt-1">Sella body butter</p>
-                              <p class="py-2">Lip Care</p>
-                              <p class="pb-4">$2000</p>
-                              <!-- switch button -->
-                              <div class="d-flex justify-content-between">
-                                <p>inactive</p>
-                                <!-- Checked switch -->
-                                  <div class="form-check form-switch">
-                                    <input class="InputStyle form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked />
-                                  </div>
-                                <!-- end checked switch -->
-                                <p class="ps-3">Active</p>
-                              </div>
-                            </div>
-                          </div>
-                          </section>
-                        </div>
-                      </div>
-                    </div>
-                </div>
-            </div>
+  <div class="container-full">
+    <div class="row">
+      <div class="col-lg-3 col-md-3">
+        <!-- Categories -->
+        <section class="CatSec mt-4 py-2 px-4">
+          <div class="d-flex flex-row align-items-center py-3">
+            <img
+              src="~/assets/pictures/categorie.svg"
+              class="CatImg"
+              alt="categorie"
+            />
+            <p class="px-2 CatTxt m-0">Categories</p>
+          </div>
+          <!-- categories details -->
+          <div class="d-flex align-items-center justify-content-between">
+            <p class="CategorieTxt">Accesories</p>
+            <p>(5)</p>
+          </div>
+          <hr />
+          <div class="pt-3 d-flex align-items-center justify-content-between">
+            <p class="CategorieTxt">Face care</p>
+            <p>(5)</p>
+          </div>
+          <hr />
+          <div class="pt-3 d-flex align-items-center justify-content-between">
+            <p class="CategorieTxt">Lip care</p>
+            <p>(5)</p>
+          </div>
+          <hr />
+          <div class="pt-3 d-flex align-items-center justify-content-between">
+            <p class="CategorieTxt">Skin care</p>
+            <p>(5)</p>
+          </div>
+          <hr />
+          <div class="pt-3 d-flex align-items-center justify-content-between">
+            <p class="CategorieTxt">Nail care</p>
+            <p>(5)</p>
+          </div>
+          <hr />
+          <div class="pt-3 d-flex align-items-center justify-content-between">
+            <p class="CategorieTxt">Blusher</p>
+            <p>(5)</p>
+          </div>
+        </section>
+        <!-- Popular Tags -->
+        <section class="CatSec mt-4 py-2 px-4">
+          <div class="d-flex flex-row align-items-center py-3">
+            <img
+              src="~/assets/pictures/categorie.svg"
+              class="CatImg"
+              alt="categorie"
+            />
+            <p class="px-2 CatTxt m-0">Popular Tags</p>
+          </div>
+          <hr />
+          <!-- categories details -->
+          <div class="d-flex align-items-center justify-content-center">
+            <button
+              type="button"
+              class="mt-3 mx-2 btn btn-sm btn-light btn-rounded"
+            >
+              Beauty
+            </button>
+            <button
+              type="button"
+              class="mt-3 mx-2 btn btn-sm btn-light btn-rounded"
+            >
+              MakeupArtist
+            </button>
+          </div>
+          <div class="py-3 d-flex align-items-center justify-content-center">
+            <button type="button" class="mx-2 btn btn-sm btn-light btn-rounded">
+              Makeup
+            </button>
+            <button type="button" class="mx-2 btn btn-sm btn-light btn-rounded">
+              Hair
+            </button>
+            <button type="button" class="mx-2 btn btn-sm btn-light btn-rounded">
+              Nails
+            </button>
+          </div>
+          <div class="pb-3 d-flex align-items-center justify-content-center">
+            <button type="button" class="mx-2 btn btn-sm btn-light btn-rounded">
+              Hairstyle
+            </button>
+            <button type="button" class="mx-2 btn btn-sm btn-light btn-rounded">
+              Skincare
+            </button>
+          </div>
+        </section>
+      </div>
+      <div class="col-lg-9 col-md-9 mt-3">
+        <div class="d-flex justify-content-between align-items-center">
+          <AdminPagination />
+          <!-- categorie and product btn -->
+          <div class="d-flex align-items-center">
+            <button
+              type="button"
+              @click="NewGroupingStatus(true)"
+              class="BtnStyleGroup btn btn-sm mx-3"
+            >
+              Add New Grouping
+            </button>
+            <button
+              type="button"
+              @click="productFrom(true)"
+              class="BtnStyleProd btn btn-sm"
+            >
+              Add New Products
+            </button>
+          </div>
         </div>
+        <!-- admin products -->
+        <div class="AdminProd container-ful mt-2 px-4">
+          <!-- results count -->
+          <div class="d-flex align-items-center justify-content-between pt-4">
+            <!-- result number -->
+            <div>
+              <p>Showing 08 Results</p>
+            </div>
+            <!-- sort by -->
+            <div class="d-flex align-items-center">
+              <p class="sort">Sort</p>
+              <p class="SortStatus px-2">Last Opened</p>
+            </div>
+          </div>
+          <!-- end result count -->
+          <div class="container-full">
+            <div class="row">
+              <div class="col-lg-6 col-md-6 p-0 mt-5 px-2">
+                <section class="ProductBkg">
+                  <div class="d-flex justify-content-between product">
+                    <img
+                      src="~/assets/pictures/ProductOne.jpg"
+                      class="ProductImg"
+                      alt="product"
+                    />
+                    <div class="ps-2">
+                      <p class="mt-1">Sella body butter</p>
+                      <p class="py-2">Lip Care</p>
+                      <p class="pb-4">$2000</p>
+                      <!-- switch button -->
+                      <div class="d-flex justify-content-between">
+                        <p>inactive</p>
+                        <!-- Checked switch -->
+                        <div class="form-check form-switch">
+                          <input
+                            class="InputStyle form-check-input"
+                            type="checkbox"
+                            role="switch"
+                            id="flexSwitchCheckChecked"
+                            checked
+                          />
+                        </div>
+                        <!-- end checked switch -->
+                        <p class="ps-3">Active</p>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+              </div>
+              <div class="col-lg-6 col-md-6 p-0 mt-5 px-2">
+                <section class="ProductBkg">
+                  <div class="d-flex justify-content-between product">
+                    <img
+                      src="~/assets/pictures/ProductOne.jpg"
+                      class="ProductImg"
+                      alt="product"
+                    />
+                    <div class="ps-2">
+                      <p class="mt-1">Sella body butter</p>
+                      <p class="py-2">Lip Care</p>
+                      <p class="pb-4">$2000</p>
+                      <!-- switch button -->
+                      <div class="d-flex justify-content-between">
+                        <p>inactive</p>
+                        <!-- Checked switch -->
+                        <div class="form-check form-switch">
+                          <input
+                            class="InputStyle form-check-input"
+                            type="checkbox"
+                            role="switch"
+                            id="flexSwitchCheckChecked"
+                            checked
+                          />
+                        </div>
+                        <!-- end checked switch -->
+                        <p class="ps-3">Active</p>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Teleport to="body">
+        <div
+          class="w-100 vh-100 position-absolute top-0 left-0 d-flex flex-column justify-content-center align-items-center modal"
+          style="backdrop-filter: blur(4px); z-index: 1000"
+          v-if="groupingStatus"
+        >
+          <NewGrouping @close="NewGroupingStatus(false)" />
+        </div>
+      </Teleport>
+      <Teleport to="body">
+        <div
+          class="w-100 vh-100 position-absolute top-0 left-0 d-flex flex-column justify-content-center align-items-center modal"
+          style="backdrop-filter: blur(4px); z-index: 1000"
+          v-if="productFromStatus"
+        >
+          <NewProduct @close="productFrom(false)" />
+        </div>
+      </Teleport>
     </div>
+  </div>
 </template>
 
 <script>
 import AdminPagination from "@/components/AdminPagination";
+import NewGrouping from "@/components/admin/NewGrouping.vue";
+import NewProduct from "@/components/admin/NewProduct.vue";
+
 export default {
-name: "admin",
-layout: 'admin', 
-components: { AdminPagination },
-}
+  name: "admin",
+  layout: "admin",
+  components: { AdminPagination, NewGrouping, NewProduct },
+  data() {
+    return {
+      groupingStatus: false,
+      productFromStatus: false,
+    };
+  },
+  methods: {
+    NewGroupingStatus(status) {
+      this.groupingStatus = status;
+    },
+    productFrom(status) {
+      this.productFromStatus = status;
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 .InputStyle:focus {
-    box-shadow:none !important;
-    background-image: none !important;
+  box-shadow: none !important;
+  background-image: none !important;
 }
 
 .InputStyle::before {
-    box-shadow:none !important;
-    background-image: none !important;
+  box-shadow: none !important;
+  background-image: none !important;
 }
 
 .form-switch .form-check-input:after {
@@ -179,16 +273,15 @@ components: { AdminPagination },
 .form-check-input:checked:focus {
   background-color: #055452;
   transition: none;
-   border: none;
+  border: none;
 }
- 
-.form-switch .form-check-input:checked[type=checkbox]:after {
+
+.form-switch .form-check-input:checked[type="checkbox"]:after {
   background-color: #e99d7b;
   outline: 0;
   box-shadow: 0;
   border: none;
 }
-
 
 .ProductBkg {
   background-color: #fff;
@@ -203,11 +296,11 @@ components: { AdminPagination },
 }
 
 .product p {
- margin: 0;
+  margin: 0;
 }
 
 .form-check {
-  padding-left: 0;  
+  padding-left: 0;
   margin: 0;
 }
 
@@ -236,7 +329,7 @@ components: { AdminPagination },
   min-height: 70vh;
 }
 
-.BtnStyleGroup{
+.BtnStyleGroup {
   border: solid 1px #055452;
   box-shadow: none;
   color: #055452;
@@ -246,8 +339,7 @@ components: { AdminPagination },
   border-radius: 2px;
 }
 
-
-.BtnStyleProd{
+.BtnStyleProd {
   box-shadow: none;
   background-color: #e99d7b;
   color: #fff;
@@ -255,7 +347,7 @@ components: { AdminPagination },
   border-radius: 2px;
 }
 
-hr{
+hr {
   height: 1px !important;
   color: #acaaaa !important;
   width: 100%;
@@ -283,7 +375,7 @@ button {
 }
 
 .CategorieTxt {
-  font-family: 'Open Sans';
+  font-family: "Open Sans";
   font-size: 11pt;
 }
 
@@ -298,7 +390,7 @@ button {
 }
 
 .CatTxt {
-  font-family: 'Open Sans', sans-serif;
+  font-family: "Open Sans", sans-serif;
   font-size: 13pt;
   color: #231942;
 }
---
+++ pages/privacypolicy/index.vue
@@ -0,0 +1,120 @@
+<template>
+  <div>
+    <TheHeader />
+    <section class="PageBkg">
+      <div class="container px-5 px-md-4 px-lg-4 pt-4">
+        <p class="PageTitle">Privacy Policy</p>
+      </div>
+      <div class="container px-5 px-md-4 px-lg-4 pt-2">
+        <p class="Desc">
+          Kandaka Collections values every single customer. We try our best to
+          provide excellent products and services.
+        </p>
+      </div>
+      <div class="container px-5 px-md-4 px-lg-4 pt-2">
+        <p class="DescTitle">Our Privacy policy outlines:</p>
+        <ul class="pl-4">
+          <li class="Desc">
+            How and why we collect customer’s personal information.
+          </li>
+          <li class="Desc">
+            How customer’s personal information is used and protected.
+          </li>
+          <li class="Desc">
+            How can a customer opt out of their personal information being
+            collected.
+          </li>
+          <li class="Desc">
+            How to contact Kandaka Collections if any questions or concerns are
+            raised about our privacy policy.
+          </li>
+        </ul>
+      </div>
+      <div class="container px-5 px-md-4 px-lg-4 pt-4">
+        <p class="Desc">
+          The information shared by customers through registering an account at
+          the Kandaka Collections website allows us to know what types of
+          products our customers need and allows us to fulfill orders, send
+          newsletters, and respond to customer’s inquiries and requests.
+        </p>
+      </div>
+      <div class="container px-5 px-md-4 px-lg-4">
+        <p class="Desc">
+          Personal information is collected when a customer makes an account
+          with Kandaka Collections. The information provided by the customer and
+          collected by Kandaka Collections website include customer first and
+          last name, address, email, phone number, and credit card information.
+          Additionally, our website collects information on a customer’s
+          purchase history on our website. All this information is saved and
+          protected on the Kandaka Collections website and will not be shared
+          with external parties or companies not related to Kandaka Collections.
+        </p>
+      </div>
+      <div class="container px-5 px-md-4 px-lg-4">
+        <p class="Desc">
+          If customers no longer want to be serviced by Kandaka Collections or
+          have their personal information shared with us, they are free to
+          cancel their account with Kandaka Collections and unsubscribe on our
+          email.
+        </p>
+      </div>
+      <div class="container px-5 px-md-4 px-lg-4">
+        <p class="Desc">
+          If customers have questions or concerns about our privacy policy, they
+          can contact us our mail at kandakacollections@gmail.com or send a
+          message on the Kandaka Collections website through our contact page.
+        </p>
+      </div>
+    </section>
+    <TheFooter />
+  </div>
+</template>
+<script>
+import TheHeader from "@/components/Navigation/TheSubpageHeader";
+import TheFooter from "@/components/TheFooter";
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter,
+  },
+};
+</script>
+<style scoped>
+.PageBkg {
+  background-image: url("~assets/pictures/PageBack.jpg");
+  background-size: cover;
+  background-position: top;
+  background-repeat: no-repeat;
+}
+.Desc {
+  font-family: "Open Sans";
+  font-size: 11pt;
+  color: #707070;
+}
+
+.Desc:first-letter {
+  text-transform: capitalize;
+}
+.DescTitle {
+  color: #055452;
+  font-family: "Open Sans";
+  font-size: 14pt;
+  font-weight: 600;
+}
+
+.DescTitle:first-letter {
+  text-transform: capitalize;
+}
+.PageTitle {
+  font-family: "Open Sans";
+  font-size: 18pt;
+  font-weight: 600;
+  color: #231942;
+  text-transform: capitalize;
+}
+
+.PageTitle:first-letter {
+  text-transform: capitalize;
+}
+</style>
